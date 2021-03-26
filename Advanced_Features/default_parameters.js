a13fd42644264b035aae467cacadb7c91cae5c17
Simon <bardrabbit@gmail.com>
2021-03-26 17:30:22 -0400

    add default parameters

---
+++ Advanced_Features/default_parameters.js
@@ -0,0 +1,32 @@
+function greet(
+  firstName,
+  lastName,
+  salutation = 'Hi',
+  fullName = `${firstName} ${lastName}`,
+) {
+  //salutation = salutation || 'Hi';
+  console.log(`${salutation}, ${firstName} ${lastName}: ${fullName} `);
+}
+
+// greet('Simon', 'Sun');
+// greet('Simon', 'Sun', undefined);
+// greet('Simon', 'Sun', null);
+// greet('Simon', 'Sun');
+
+/* use undefine as a placeholder, but not good practice. It is better to put default parameters at the end */
+function sayHello(firstName = 'Simon', lastName, greeting = 'Nihao') {
+  console.log(`${firstName} ${lastName}, ${greeting}`);
+}
+
+// sayHello(undefined, 'Sun');
+
+/* use object instead of backtics in default parameters */
+function greeting(
+  firstName,
+  lastName,
+  { salutation = 'Mr', suffix = 'Hi' } = {},
+) {
+  console.log(`${salutation} ${suffix} ${firstName} ${lastName}`);
+}
+
+greeting('Simon', 'Sun');
