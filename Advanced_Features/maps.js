39cc1b46ffb85d2932a5b732176fb604fc1449ca
Simon <bardrabbit@gmail.com>
2021-03-28 00:05:59 -0400

    add map object comparison and map practice

--- Advanced_Features/maps.js
+++ Advanced_Features/maps.js
@@ -43,3 +43,74 @@ const userMap = new Map([[user, { age: 30 }]]);
 console.log(userMap.get(user));
 
 /* Comparison between object and map */
+
+// 1. loop through a map vs. loop through an object
+// The order of the value in the map is always the same as the order you add them into the map
+const myMap = new Map([
+  [1, 'A'],
+  [2, 'B'],
+  [3, 'C'],
+  [4, 'D'],
+]);
+
+myMap.forEach((key, value) => {
+  console.log(key, value);
+});
+
+// However, to iterate an object
+let obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+};
+
+Object.entries(obj).forEach(([key, value]) => console.log(key, value));
+
+// 2. size
+console.log(myMap.size);
+
+// Map functions
+myMap.set(5, 'E');
+
+console.log(myMap.get(5));
+console.log(myMap);
+
+console.log(map.has(6));
+
+map.delete(1);
+console.log(map);
+
+map.clear();
+console.log(map);
+
+/* Practice */
+const items = [
+  {
+    id: 1,
+    name: 'test1',
+    description: 'desc1',
+  },
+  {
+    id: 2,
+    name: 'test2',
+    description: 'desc2',
+  },
+  {
+    id: 2,
+    name: 'test2',
+    description: 'desc2',
+  },
+];
+
+function getItem(id) {
+  return items.find((item) => items.id === id);
+}
+
+const itemsMap = new Map([
+  [1, { name: 'test1', description: 'desc1' }],
+  [2, { name: 'test2', description: 'desc2' }],
+  [3, { name: 'test3', description: 'desc3' }],
+]);
+
+console.log(itemsMap.get(1));
