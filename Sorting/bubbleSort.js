11b69c74f3c867d0caf634324b4a7749a0e6596e
Simon <bardrabbit@gmail.com>
2021-04-13 13:13:41 -0400

    optimize bubblesort

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,11 +1,16 @@
 const bubbleSort = (arr) => {
+  let swap;
   for (let i = arr.length - 1; i > 0; i--) {
+    swap = false;
     for (let j = 0; j < i; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swap = true;
       }
     }
+    if (!swap) break;
   }
   return arr;
 };
-console.log(bubbleSort([5, 4, 2, 3, -1, 7, 19]));
+console.log(bubbleSort([8, 1, 2, 3, 4, 5]));
